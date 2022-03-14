af97ac1def16ff7148dc119cd11963838572cd84
Julia Queiroz <91906999+juqueiroza@users.noreply.github.com>
2022-03-14 20:21:10 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,20 +3,23 @@
 
 int a[50], size, min, max, avg;
 
-void *thread_avg() {
-	float sum = 0;
+void *th (){
         int i;
 
 	printf("Enter number of elements in array: ");
 	scanf("%d", &size);
 
-	for(i = 0; i< size; i++) {
-	        printf("number %d -> ", i + 1);
+	for(i = 1; i < size; i++) {
+	        printf("number %d -> ", i);
 	        scanf("%d", &a[i]);
                 printf("\n");
 	}
 
-        i = 0;
+}
+
+void *thread_avg() {
+        int sum = 0;
+        int i = 0;
 	for(i = 0; i < size; i++){
 		sum = sum + a[i];
         }
@@ -27,7 +30,7 @@ void *thread_avg() {
 
 void *thread_min() {
 	int temp = a[0];
-        int i = 1;
+        int i;
 	for(i = 1; i < size; i++) {
 		if(temp > a[i]) {
 			temp = a[i];
@@ -50,15 +53,18 @@ void *thread_max() {
 	printf("\nThe maximum value is %d",max);
 }
 
-int main()
-{
+int main() {
+
     pthread_t th1;
     pthread_t th2;
     pthread_t th3;
-	pthread_create(&th1, NULL, &thread_avg, NULL);
-	pthread_join(th1,NULL);
-	pthread_create(&th2, NULL, &thread_min, NULL);
-        pthread_join(th2,NULL);
-	pthread_create(&th3, NULL, &thread_max, NULL);
-        pthread_join(th3,NULL);
+    pthread_t th4; 
+        pthread_create(&th1, NULL, &th, NULL);
+        pthread_join(th1, NULL);
+	pthread_create(&th2, NULL, &thread_avg, NULL);
+	pthread_join(th2, NULL);
+	pthread_create(&th3, NULL, &thread_min, NULL);
+        pthread_join(th3, NULL);
+	pthread_create(&th4, NULL, &thread_max, NULL);
+        pthread_join(th4, NULL);
 }
