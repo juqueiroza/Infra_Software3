df48180a8241899b8a6704c1d33fc99915ed1b0e
Julia Queiroz <91906999+juqueiroza@users.noreply.github.com>
2022-03-14 21:24:34 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,27 +3,14 @@
 
 int a[50], size, min, max, avg;
 
-void *th (){
-        int i;
-
-	printf("Enter number of elements in array: ");
-	scanf("%d", &size);
-
-	for(i = 0; i < size; i++) {
-	        scanf("%d", &a[i]);
-	}
-
-}
-
 void *thread_avg() {
         float sum = 0;
         int i;
 	for(i = 0; i < size; i++){
 		sum = sum + a[i];
         }
-
 	avg = sum/size;
-	printf("The average value is: %d",avg);
+        pthread_exit (NULL);
 }
 
 void *thread_min() {
@@ -35,8 +22,7 @@ void *thread_min() {
 		}
 	}
         min = temp;
-	printf("\nThe minimum value is %d",min);
-
+        pthread_exit (NULL);
 }
 
 void *thread_max() {
@@ -48,21 +34,37 @@ void *thread_max() {
 		}
 	}
         max = temp;
-	printf("\nThe maximum value is %d",max);
+        pthread_exit (NULL);
 }
 
-int main() {
+void *th(void *arg){
+        int i;
 
-    pthread_t th1;
-    pthread_t th2;
-    pthread_t th3;
-    pthread_t th4; 
-        pthread_create(&th1, NULL, &th, NULL);
-        pthread_join(th1, NULL);
-	pthread_create(&th2, NULL, &thread_avg, NULL);
+	printf("Enter number of elements in array: ");
+	scanf("%d", &size);
+
+	for(i = 0; i < size; i++) {
+	        scanf("%d", &a[i]);
+	}
+
+        pthread_t th2;
+        pthread_t th3;
+        pthread_t th4; 
+        pthread_create(&th2, NULL, &thread_avg, NULL);
 	pthread_join(th2, NULL);
 	pthread_create(&th3, NULL, &thread_min, NULL);
         pthread_join(th3, NULL);
 	pthread_create(&th4, NULL, &thread_max, NULL);
         pthread_join(th4, NULL);
+
+        printf("The average value is: %d",avg);
+        printf("\nThe minimum value is %d",min);
+        printf("\nThe maximum value is %d\n",max);
+        pthread_exit (NULL);
+}
+
+int main() {
+        pthread_t th1;
+        pthread_create(&th1, NULL, &th, NULL);
+        pthread_join(th1, NULL);
 }
